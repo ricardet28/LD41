a4647c01741e82604eef783cb6089de9872c9f48
Ricky <lacajadericky@gmail.com>
2018-04-21 13:44:16 +0200

    rotacion arma creada

--- Assets/GunAiming.cs
+++ Assets/GunAiming.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class GunAiming : MonoBehaviour {
 
+    public float speedRotation;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +13,26 @@ public class GunAiming : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        Aiming();
 	}
+
+    private void Aiming()
+    {
+        /*
+        Debug.Log("aiming!!");
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.position.z));
+        Vector3 direction = mousePosition - transform.position;
+        Quaternion rotationObjective = Quaternion.LookRotation(direction);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotationObjective, speedRotation * Time.deltaTime);
+        //transform.LookAt(direction);
+        */
+
+        float rotation = Input.GetAxis("Vertical");
+        transform.Rotate(0f, 0f,  rotation * Time.deltaTime * speedRotation);
+        Debug.Log(rotation);
+    }
+
+
+        
+
 }
