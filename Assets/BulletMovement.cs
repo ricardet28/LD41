cf01a6d3db40b7ad4c003577ba469cd489b9df19
Ricky <lacajadericky@gmail.com>
2018-04-21 12:00:28 +0200

    pruebas

---
+++ Assets/BulletMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMovement : MonoBehaviour {
+    private Rigidbody rb;
+    // Use this for initialization
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void bulletMovement()
+    {
+        rb.AddForce(Vector3.up * 30f);
+    }
+    
+
+}
